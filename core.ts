43d28cecffb864f80178fd351c0e59a67ed6d03a
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-06-22 14:28:53 +0000

    IDK What I'm doing atp

--- core.ts
+++ core.ts
@@ -1,5 +1,5 @@
 const linkRegex =
-    /.*(https?:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/(.+?)#L(\d+)).*/g;
+    /(https?:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/(.+?)#L(\d+))/g;
 export function containsGithubLink(message: string): boolean {
     return linkRegex.test(message.replaceAll("\n", " "));
 }
