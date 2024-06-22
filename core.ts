202837e127bfa702e4378e909b8d674c8d3ff851
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-06-22 14:53:41 +0000

    Fix bug with more regex statefulness + Add indicator of selected line

--- core.ts
+++ core.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 const linkRegex =
     /(https?:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/(.+?)#L(\d+))/g;
 export function containsGithubLink(message: string): boolean {
-    return linkRegex.test(message.replaceAll("\n", " "));
+    return /(https?:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/(.+?)#L(\d+))/g.test(
+        message.replaceAll("\n", " ")
+    );
 }
 
-const linesSpan = 5;
+const linesSpan = 10;
 
 export async function prepareReply(message: string): Promise<string> {
     const parse =
@@ -22,8 +24,17 @@ export async function prepareReply(message: string): Promise<string> {
         const lines = response.data.split("\n");
         const start = Math.max(0, lineNumber - linesSpan);
         const end = Math.min(lines.length, lineNumber + linesSpan - 1);
+        lines[lineNumber - 1] = `>>> ${lines[lineNumber - 1]}`;
         const codeSnippet = lines.slice(start, end).join("\n");
         return codeSnippet;
     });
+
+    const fileName = parse![5];
+    if (!fileName) {
+        throw new Error("Couldn't find filename in the URL");
+    }
+    const extension = fileName.split(".").pop();
+    snippet = `\`\`\`${extension}\n${snippet}\n\`\`\``;
+
     return snippet;
 }
